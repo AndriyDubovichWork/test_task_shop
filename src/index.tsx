f55970a612a5563d5a975d083b269ce990b2e3c7
AndriyDubovichWork <86286088+AndriyDubovichWork@users.noreply.github.com>
2023-02-11 19:20:48 +0200

    api url

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { StylesProvider } from '@material-ui/core/styles';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import { StylesProvider } from '@material-ui/core/styles';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 const client = new ApolloClient({
-  uri: 'https://graphql-shop-api.herokuapp.com/',
-  cache: new InMemoryCache(),
+	uri: 'https://shop-api-andriydubovichwork.vercel.app/',
+	cache: new InMemoryCache(),
 });
 
 root.render(
-  <ApolloProvider client={client}>
-    <StylesProvider injectFirst={false}>
-      <RecoilRoot>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </RecoilRoot>
-    </StylesProvider>
-  </ApolloProvider>
+	<ApolloProvider client={client}>
+		<StylesProvider injectFirst={false}>
+			<RecoilRoot>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</RecoilRoot>
+		</StylesProvider>
+	</ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
